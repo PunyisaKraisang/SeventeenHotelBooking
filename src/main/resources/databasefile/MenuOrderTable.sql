96d03c65f408ccec09b70b4387312a5f29f9703c
Punyisa Kraisang <punyisa.kra@gmail.com>
2020-10-10 23:37:55 -0700

    Add menu_order_item table is db

--- src/main/resources/databasefile/MenuOrderTable.sql
+++ src/main/resources/databasefile/MenuOrderTable.sql
@@ -11,6 +11,16 @@ CREATE TABLE Menu_Order (
     CONSTRAINT fk_reservation_id_for_food FOREIGN KEY (Reservation_id) REFERENCES Room_Reservation(Reservation_id)
 );
 
+CREATE TABLE Menu_Order_Item (
+    Order_id NUMBER,
+    Menu_id NUMBER,
+    Quantity NUMBER,
+    Deviler_datetime DATE,
+    Status NVARCHAR2(10),
+    CONSTRAINT pk_menu_order_item_id PRIMARY KEY(Order_id, Menu_id),
+    CONSTRAINT fk_reservation_id_for_food FOREIGN KEY (Order_id) REFERENCES Menu_Order(Order_id)
+);
+
 -- Insert data
 INSERT INTO Menu_Order VALUES (1, 1, '07-OCT-2020', 3.52, 3.00, 27.54);
 
