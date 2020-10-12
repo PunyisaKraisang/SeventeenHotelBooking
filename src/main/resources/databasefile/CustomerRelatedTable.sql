69e0d6d74f42babc6e98fa04f13e7ca10819a6a0
Feifan Wu <FeifanWu96@gmail.com>
2020-10-11 23:26:13 -0500

    customer id added

--- src/main/resources/databasefile/CustomerRelatedTable.sql
+++ src/main/resources/databasefile/CustomerRelatedTable.sql
@@ -1,24 +1,27 @@
 drop table customer CASCADE CONSTRAINTS;
 
 CREATE TABLE customer (
-    username nvarchar2(10) primary key,
+    customer_id number primary key,
+    username nvarchar2(10),
     first_name nvarchar2(20),
     last_name nvarchar2(20),
     birth_date DATE,
     phone nvarchar2(20),
     email_address nvarchar2(30)
 );
-
+create sequence customer_id_seq 
+start with 100
+increment by 1;
 CREATE TABLE account (
     username nvarchar2(10) primary key,
     password nvarchar2(20) not null
 );
 
 -- create dummy customer
-insert into customer (username, first_name, last_name, birth_date, phone, email_address) 
-values ( 'dummy', 'Dummif', 'Dummilastier', '01-JAN-1990', '1111111111', 'dummy@email.com');
+insert into customer (customer_id, username, first_name, last_name, birth_date, phone, email_address) 
+values ( customer_id_seq.nextval, 'dummy', 'Dummif', 'Dummilastier', '01-JAN-1990', '1111111111', 'dummy@email.com');
 
 insert into account (username, password) 
 values ( 'dummy', 'dummy1234');
-
 select * from customer;
+select * from account;
